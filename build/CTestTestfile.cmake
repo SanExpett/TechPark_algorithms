392c90e9309f8acf875512bc42105d98228f6aba
SanExpett <118511246+SanExpett@users.noreply.github.com>
2023-04-09 15:12:28 +0300

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/alexey/Рабочий стол/TPark/Algorithms/task1
-# Build directory: /home/alexey/Рабочий стол/TPark/Algorithms/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
